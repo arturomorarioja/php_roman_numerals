5efdba3416b3d24541cacc4586a02749aeaa91d0
Arturo Mora-Rioja <arturomora@gmail.com>
2024-09-05 19:58:56 +0200

    Specification checks added

--- src/conversion.php
+++ src/conversion.php
@@ -5,14 +5,73 @@
  * 
  * @author  Arturo Mora-Rioja
  * @version 1.0.0 January 2023
+ * @version 1.1.0 September 2024. Specification checks added
  */
 
 function roman2decimal(string $number): int {
 
-    // Each letter of the Roman numeral is translated into its decimal value
+    // Invalid symbols are removed from the string
     $number = str_split($number);
-    $digits = [];
+    $cleanNumber = [];
     foreach ($number as $letter) {
+        if (in_array($letter, ['M', 'D', 'C', 'L', 'X', 'V', 'I'])) {
+            $cleanNumber[] = $letter;
+        }
+    }
+    $number = implode($cleanNumber);
+
+    // V, L and D can never be repeated
+    if (substr_count($number, 'V') > 1 || 
+        substr_count($number, 'L') > 1 || 
+        substr_count($number, 'D') > 1) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // No digit can be repeated more than 3 times in a row
+    if (str_contains($number, 'MMMM') ||
+        str_contains($number, 'CCCC') ||
+        str_contains($number, 'XXXX') ||
+        str_contains($number, 'IIII')) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // D cannot precede M
+    if (str_contains($number, 'DM')) {
+        throw new Exception('Badly formed Roman numeral');
+    }
+
+    // L cannot precede M, D or C
+    if (str_contains($number, 'LM') ||
+        str_contains($number, 'LD') ||
+        str_contains($number, 'LC')) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // V cannot precede M, D, C, L or X
+    if (str_contains($number, 'VM') ||
+        str_contains($number, 'VD') ||
+        str_contains($number, 'VC') ||
+        str_contains($number, 'VL') ||
+        str_contains($number, 'VX')) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // X cannot precede D
+    if (str_contains($number, 'XD')) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // I cannot precede M, D, C, L
+    if (str_contains($number, 'IM') ||
+        str_contains($number, 'ID') ||
+        str_contains($number, 'IC') ||
+        str_contains($number, 'IL')) {
+            throw new Exception('Badly formed Roman numeral');
+    }
+
+    // Each letter of the Roman numeral is translated into its decimal value
+    $digits = [];
+    foreach ($cleanNumber as $letter) {
         switch($letter) {
             case 'M': $digits[] = 1000; break;
             case 'D': $digits[] = 500; break;
@@ -21,7 +80,6 @@ function roman2decimal(string $number): int {
             case 'X': $digits[] = 10; break;
             case 'V': $digits[] = 5; break;
             case 'I': $digits[] = 1; break;
-            default: break; // Incorrect letters are simply ignored
         }
     }
 
@@ -40,4 +98,3 @@ function roman2decimal(string $number): int {
 
     return $number;
 }
-
--- tests/conversionTest.php
+++ tests/conversionTest.php
@@ -7,16 +7,16 @@ use PHPUnit\Framework\TestCase;
 class conversionTest extends TestCase {
 
     /**
-     * @dataProvider provideRoman2Decimal
+     * @dataProvider provide_roman_to_decimal
      */
-    public function testRoman2DecimalConversion($value, $expected): void {
+    public function test_roman_to_decimal_passes($value, $expected): void {
         $result = roman2decimal($value);
 
         $this->assertEquals($expected, $result);
     }
-    public function provideRoman2Decimal() {
+    public function provide_roman_to_decimal() {
         return [
-            ['', 0],            
+            ['', 0],                // Valid lower boundary
             ['M', 1000],            
             ['MD', 1500],            
             ['MCD', 1400],
@@ -28,8 +28,54 @@ class conversionTest extends TestCase {
             ['IX', 9],
             ['MMXXIII', 2023],
             ['MMJXXOIII', 2023],
-            ['MXDIVII', 1496],
+            ['MMMCMXCIX', 3999],    // Valid upper boundary
         ];
     }
-
+    
+    /**
+     * @dataProvider provide_badly_formed_roman_numerals
+     */
+    public function test_badly_formed_roman_numerals($value): void {
+        $this->expectExceptionMessage('Badly formed');
+        $result = roman2decimal($value);
+    }
+    public function provide_badly_formed_roman_numerals() {
+        return [
+            ['MXDIVII', false],
+            ['VV', false],
+            ['MDVVI', false],
+            ['MDVXVI', false],
+            ['LL', false],
+            ['MDLLX', false],
+            ['MDLXLV', false],
+            ['DD', false],
+            ['MDDCX', false],
+            ['MDCDXVI', false],
+            ['IIII', false],
+            ['XVIIII', false],
+            ['CLIIIIX', false],
+            ['XXXX', false],
+            ['MDXXXX', false],
+            ['MCXXXXV', false],
+            ['CCCC', false],
+            ['MDCCCC', false],
+            ['MCCCCDV', false],
+            ['MMMM', false],        // Invalid upper boundary
+            ['MMMMCL', false],
+            ['DM', false],
+            ['LM', false],
+            ['LD', false],
+            ['LC', false],
+            ['VM', false],
+            ['VD', false],
+            ['VC', false],
+            ['VL', false],
+            ['VX', false],
+            ['XD', false],
+            ['IM', false],
+            ['ID', false],
+            ['IC', false],
+            ['IL', false],
+        ];
+    }
 }
